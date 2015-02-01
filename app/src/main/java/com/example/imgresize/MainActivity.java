047a913064e8fd0ed912237551ca8953ee062108
andrewcfc <andrewzhidenko@gmail.com>
2015-02-01 16:02:17 +0200

    Added Notifications

--- app/src/main/java/com/example/imgresize/MainActivity.java
+++ app/src/main/java/com/example/imgresize/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.imgresize;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,52 +19,72 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.imgresize.data.model.DownloadImage;
-import com.example.imgresize.data.model.ImageModel;
-import com.example.imgresize.data.model.data.assets.Parsing;
-
-import org.json.JSONException;
 
 public class MainActivity extends ActionBarActivity {
 
-    public ArrayList<ImageModel> links = new ArrayList<>();
     ListView imageList;
-
+    public static final String ACTION = "com.example.imageresize.DOWNLOADING_BITMAPS";
+    public static final String PARAMS = "images";
+    ImageReceive receive;
+    ArrayList<String> paths = new ArrayList<>();
+    ArrayList<Bitmap> images = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        try {
-            links = Parsing.parseJSON(this);
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter(ACTION);
+        receive = new ImageReceive();
+        registerReceiver(receive, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receive);
+    }
+
+    public void startDownloading(View view) {
+        imageList = (ListView) findViewById(R.id.listView);
+        startService(new Intent(MainActivity.this, DownloadImage.class));
     }
 
 
 
-    public void startDownloading(View view) {
-        imageList = (ListView) findViewById(R.id.listView);
+    //---------------------RECEIVER-------------------------
+    public class ImageReceive extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            paths = intent.getStringArrayListExtra(PARAMS);
 
-        new DownloadImage(MainActivity.this, new DownloadImage.IOnPicturesDownloader() {
-            @Override
-            public void onImagesDownloaded(ArrayList<Bitmap> images) {
-                ImageAdapter adapter = new ImageAdapter(MainActivity.this, images);
-                imageList.setAdapter(adapter);
-                imageList.setVisibility(View.VISIBLE);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+            for(int i=0; i<paths.size(); i++) {
+                images.add(BitmapFactory.decodeFile(paths.get(i), options));
             }
-        }).execute(links);
+
+            ImageAdapter adapter = new ImageAdapter(MainActivity.this, images);
+            imageList.setAdapter(adapter);
+            imageList.setVisibility(View.VISIBLE);
+
+            stopService(new Intent(MainActivity.this, DownloadImage.class));
+        }
     }
 
     //_________________________ADAPTOR FOR LIST_______________________________________
     public class ImageAdapter extends BaseAdapter {
         Context context;
+
         ArrayList<Bitmap> images;
 
         public ImageAdapter(Context context, ArrayList<Bitmap> imgs) {
-            //super(context, R.layout.image_row, R.id.imageInRow, imgs);
-
             this.images = imgs;
             this.context = context;
         }
@@ -99,6 +116,6 @@ public class MainActivity extends ActionBarActivity {
 
             return row;
         }
-    }
 
+    }
 }
